81ff164672106a3ebaec1790423ad8aa040e298f
garciart <rgarcia@rgprogramming.com>
2019-12-09 22:32:15 -0500

    Prepping CentOS VM and documenting in README.md.

--- demos/test.py
+++ demos/test.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 import cv2
-
+print(cv2.__version__)
 # Root directory of the project
 ROOT_DIR = Path(__file__).resolve().parent
 
@@ -11,16 +11,19 @@ ROOT_DIR = Path(__file__).resolve().parent
 IMAGE_DIR = os.path.join(ROOT_DIR, "demo_images")
 VIDEO_DIR = os.path.join(ROOT_DIR, "demo_videos")
 
+# print(cv2.getBuildInformation())
+
 # Image, video or camera to process - set this to 0 to use your webcam instead of a video file
 # FRAME_SOURCE = [(IMAGE_DIR + "\\demo_image1.jpg"),(IMAGE_DIR + "\\demo_image2.jpg"),(IMAGE_DIR + "\\demo_image3.jpg")]
 # FRAME_SOURCE = [(VIDEO_DIR + "\\demo_video1.mp4"),(VIDEO_DIR + "\\demo_video2.mp4"),(VIDEO_DIR + "\\demo_video3.mp4")]
-# FRAME_SOURCE = [(IMAGE_DIR + "\\demo_image1.jpg")]
+FRAME_SOURCE = [(IMAGE_DIR + "\\demo_image1.jpg")]
 # FRAME_SOURCE = ["http://binaryapi.ap.org/f4d6b22730794576a126bedad673656c/preview/AP19316470225288.jpg?wm=api&ver=0",]
-FRAME_SOURCE = ["https://rgprogramming.com/wp-content/uploads/wp-header-clear-280.png"]
+# FRAME_SOURCE = ["https://rgprogramming.com/wp-content/uploads/wp-header-clear-280.png",]
+# FRAME_SOURCE = ["http://www.pumphreyautobody.com/dpautobody/resources/innerfade/images/dpautobodyphoto03.jpg"]
 
 def main():
     for f in FRAME_SOURCE:
-        print(FRAME_SOURCE)
+        print(f)
         # Load the video file we want to run detection on
         video_capture = cv2.VideoCapture(f)
 
